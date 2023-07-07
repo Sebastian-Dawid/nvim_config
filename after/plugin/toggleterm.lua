fb20428dae12e43bcc7ee4f6f991ddc64ed1a60e
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2023-07-07 22:00:17 +0200

    Added ToggleTerm

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,9 @@
+require("toggleterm").setup({
+    size = 40,
+    open_mapping = [[<C-\>]],
+    hide_numbers = true,
+    shade_terminals = true,
+    shading_factor = 0.6,
+    direction = 'float',
+    shell = 'nu',
+})
--- lua/sdawid/packer.lua
+++ lua/sdawid/packer.lua
@@ -4,60 +4,62 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use({
-	  "neanias/everforest-nvim",
-	  -- Optional; default configuration will be used if setup isn't called.
-	  config = function()
-		  require("everforest").setup({
-			  background = "hard",
-			  transparent_background_level=1,
-			  italics=true,
-			  disable_italic_comments=false,
-			  sign_column_background="none",
-			  ui_contrast="low",
-			  dim_inactive_windows=false,
-			  diagnostic_text_highlight=false,
-			  diagnostic_virtual_text="coloured",
-			  diagnostic_line_highlight=false,
-			  spell_foreground=false,
-			  on_highlights=function(highlight_groups, palette) end
-		  })
-	  end,
-  })
+    use({
+        "neanias/everforest-nvim",
+        -- Optional; default configuration will be used if setup isn't called.
+        config = function()
+            require("everforest").setup({
+                background = "hard",
+                transparent_background_level=1,
+                italics=true,
+                disable_italic_comments=false,
+                sign_column_background="none",
+                ui_contrast="low",
+                dim_inactive_windows=false,
+                diagnostic_text_highlight=false,
+                diagnostic_virtual_text="coloured",
+                diagnostic_line_highlight=false,
+                spell_foreground=false,
+                on_highlights=function(highlight_groups, palette) end
+            })
+        end,
+    })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+
+    use {"akinsho/toggleterm.nvim", tag = '*'}
 end)
