7afbe63c9b87467f64de69b55caa278e4ca9a2c0
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2023-11-24 14:59:35 +0100

    Resored <C-h> shortcut in insert mode.

--- init.lua
+++ init.lua
@@ -218,6 +218,7 @@ local on_attach = function(_, bufnr)
 
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'Signature Documentation' })
 
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
